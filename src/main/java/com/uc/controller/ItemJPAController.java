8abf05da4686b8d646c231bc9312750cbf9c109b
Upkar <Upkar@DESKTOP-22L0J3T>
2023-03-08 11:30:21 +0530

    Use Jpa Service

--- src/main/java/com/uc/controller/ItemJPAController.java
+++ src/main/java/com/uc/controller/ItemJPAController.java
@@ -3,6 +3,7 @@ package com.uc.controller;
 import com.uc.Exception.ItemNotFoundException;
 import com.uc.bean.Item;
 import com.uc.service.ItemDaoService;
+import com.uc.service.jpa.ItemRepository;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,6 +11,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 /* To access swagger, URL is http://localhost:8080/swagger-ui/index.html */
 
@@ -19,28 +21,30 @@ public class ItemJPAController {
 
     //    @Autowired
     private ItemDaoService service;
+    private ItemRepository repository;
 
-    public ItemJPAController(ItemDaoService service) {
+    public ItemJPAController(ItemDaoService service, ItemRepository repository) {
         this.service = service;
+        this.repository = repository;
     }
 
     @GetMapping("/jpa/getAllItems")
     public List<Item> getAllItems() {
-        return service.getAllItems();
+        return repository.findAll();
     }
 
     @GetMapping("/jpa/getItem/{id}")
     public Item getItem(@PathVariable int id) {
-        Item item = service.getItem(id);
-        if (item == null) {
+        Optional<Item> item = repository.findById(id);
+        if (item.isEmpty()) {
             throw new ItemNotFoundException(String.format("Item with id : %s not found", id));
         }
-        return item;
+        return item.get();
     }
 
     @PostMapping("/jpa/createItem")
     public ResponseEntity<Item> createItem(@Valid @RequestBody Item item) {
-        Item createdItem = service.createItem(item);
+        Item createdItem = repository.save(item);
         URI location = ServletUriComponentsBuilder
                 .fromCurrentRequest()
                 .path("/{id}")
@@ -52,6 +56,6 @@ public class ItemJPAController {
 
     @DeleteMapping("/jpa/deleteItem/{id}")
     public void deleteItem(@PathVariable int id) {
-        service.deleteItemById(id);
+        repository.deleteById(id);
     }
 }
